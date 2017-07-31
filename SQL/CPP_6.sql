a78b86680e301f8153cd86855bc2c8c0acfcfa0c
Joshua Mumm <joshua.mumm@alaska.gov>
2017-07-31 15:11:54 -0800

    Remove NZ from estimated larges in CPP_6.  Now non sample pots will have null large estiamtes.

--- SQL/CPP_6.sql
+++ SQL/CPP_6.sql
@@ -8,12 +8,9 @@ CPP_3.SAMPLE_POT,
 CPP_3.all_Cnt as all_Cnt_cc,
 CPP_3.all_Kg as all_Kg_cc,
 
-NZ(	[CPP_3.all_Cnt] * [CPP_5.propLgCnt_awl],
-	0) AS lrg_Cnt,
+[CPP_3.all_Cnt] * [CPP_5.propLgCnt_awl] AS lrg_Cnt,
 
-Round(NZ(
-	[CPP_3.all_Kg] * [CPP_5.propLgWt_awl],
-	0),3) AS lrg_Kg
+Round([CPP_3.all_Kg] * [CPP_5.propLgWt_awl],3) AS lrg_Kg
 
 INTO CPP 
 
@@ -21,4 +18,6 @@ FROM (CPP_3
 	LEFT JOIN
 		CPP_5 ON (CPP_3.POT_ID = CPP_5.POT_ID)
 		 AND (CPP_3.EVENT_ID = CPP_5.EVENT_ID))
+
+ORDER BY CPP_3.STATION, CPP_3.SITE_ID, CPP_3.EVENT_ID, CPP_3.YEAR
 ;
