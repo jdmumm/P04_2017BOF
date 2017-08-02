a4967bfbc511fd772a1c1b693f8cd0d48b1ac955
Joshua Mumm <joshua.mumm@alaska.gov>
2017-08-02 11:07:06 -0800

    add another NZ to CPP.5 to deal with 0s as denominator

--- SQL/CPP_5.sql
+++ SQL/CPP_5.sql
@@ -2,23 +2,25 @@ SELECT DISTINCT
 
 EVENT_ID,
 POT_ID, 
-
+  
+  NZ(
   NZ(DSum("[FREQUENCY]", "[CPP_4]", "[EVENT_ID] = '" & [EVENT_ID] & "' 
 	AND [POT_ID] = '" & [POT_ID] & "'
 	AND CARAPACE_LENGTH_MM >= 32"),0)
 	/
 	NZ(DSum("[FREQUENCY]", "[CPP_4]", "[EVENT_ID] = '" & [EVENT_ID] & "' 
 	AND [POT_ID] = '" & [POT_ID] & "'
-	AND CARAPACE_LENGTH_MM >= -1"),0)
+	AND CARAPACE_LENGTH_MM >= -1"),0),0)
 	AS propLgCnt_awl,
 	
+  NZ(
   NZ(DSum("[KG]", "[CPP_4]", "[EVENT_ID] = '" & [EVENT_ID] & "' 
 	AND [POT_ID] = '" & [POT_ID] & "'
 	AND CARAPACE_LENGTH_MM >= 32"),0)
 	/
 	NZ(DSum("[KG]", "[CPP_4]", "[EVENT_ID] = '" & [EVENT_ID] & "' 
 	AND [POT_ID] = '" & [POT_ID] & "'
-	AND CARAPACE_LENGTH_MM >= -1"),0)
+	AND CARAPACE_LENGTH_MM >= -1"),0),0)
 	AS propLgWt_awl	
 
 FROM 
